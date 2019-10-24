a6b93124a79c13514a31537ebb51961cb66dad92
Abdirashid abdi <36994424+abdirashidabdi@users.noreply.github.com>
2019-10-24 14:44:49 -0400

    new filles added

---
+++ todo/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+
+
+# Create your views here.
+
+def todoView(request):
+    render(request,'todo.html')
