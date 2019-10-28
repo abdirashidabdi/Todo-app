9578de61b8e77b0df4f4458d25f1a6aaf001eac6
Abdirashid.Abdi <student.a.abdi7764@horizoncolumbus.org>
2019-10-28 09:25:38 -0400

    new lines added 5

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-
-from todo.models import TodoItem
+from .models import TodoItem
 
 
 def todoView(request):
@@ -10,13 +9,15 @@ def todoView(request):
                   {'all_items': all_todo_items})
 
 
-# def add_todo(request):
-#     new_item = TodoItem(content=request.POST['content'])
-#     new_item.save()
-#     return HttpResponseRedirect('/todo/')
+def addTodo(request):
+    new_item = TodoItem(content=request.POST['content'])
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+
 #
 #
-# def delete_todo(request, todo_id):
-#     item_to_delete = TodoItem.objects.get(id=todo_id)
-#     item_to_delete.delete()
-#     return HttpResponseRedirect('/todo/')
+def deleteTodo(request, todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+    return HttpResponseRedirect('/todo/')
